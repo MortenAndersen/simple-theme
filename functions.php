049bdd031b76ba0e445d4d9ea3b0984805d3ad40
Morten Andersen <morten@hjemmesider.dk>
2019-11-11 14:29:53 +0100

    ACF personer

--- functions.php
+++ functions.php
@@ -95,6 +95,7 @@ require get_parent_theme_file_path( '/inc/optionspage.php' );
 
 // ACF
 require get_parent_theme_file_path( '/assets/acf/acf-fields-gallery.php' );
+require get_parent_theme_file_path( '/assets/acf/acf-personer.php' );
 
 // WooCommerce
 require get_parent_theme_file_path( '/inc/woo-functions.php' );
