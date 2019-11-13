b44ce69b2bfc33162897b90dff4999eef26aa9ea
Morten Andersen <morten@hjemmesider.dk>
2019-11-13 15:49:36 +0100

    g1

--- template/page/page-loop.php
+++ template/page/page-loop.php
@@ -1,11 +1,11 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
+<?php simpleTheme_the_post_thumbnail(); ?>
 <?php  if ( !is_singular( 'person' ) ) {
 	the_title( '<h1>', '</h1>');
 }
 
-simpleTheme_the_post_thumbnail();
+
 
 if ( is_single() && !is_singular( 'person' ) ) {
 	simpleTheme_date();
