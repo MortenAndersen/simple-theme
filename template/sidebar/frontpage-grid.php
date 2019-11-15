22a1875b60b64e67112ee83244a6f3b229c72d7a
Morten Andersen <morten@hjemmesider.dk>
2019-11-15 13:36:34 +0100

    top con

--- template/sidebar/frontpage-grid.php
+++ template/sidebar/frontpage-grid.php
@@ -1,4 +1,4 @@
-<?php if ( is_front_page() ) { ?>
+<?php if ( is_front_page() && is_active_sidebar( 'sidebar-frontpage-grid' ) ) { ?>
 <div class="frontpage-grid l-main--content l-wrap flex-con g4">
 <?php dynamic_sidebar( 'sidebar-frontpage-grid' ); ?>
 </div>
