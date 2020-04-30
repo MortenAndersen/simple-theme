5605407c75ad74ef7d006ab2f02b670782706e3b
Morten Andersen <morten@hjemmesider.dk>
2020-04-30 13:25:28 +0200

    archive-loop 2.0

--- template/post/archive-loop.php
+++ template/post/archive-loop.php
@@ -1,16 +1,27 @@
 <?php if ( have_posts() ) { ?>
-	<h1><?php single_cat_title(); ?></h1>
-	<?php echo category_description(); ?>
+
+	<?php if ( is_category() ){ ?>
+		<h1><?php single_cat_title(); ?></h1>
+		<?php echo category_description(); ?>
+	<?php } ?>
+
 <div class="simple-archive flex-con g4">
 	<?php while ( have_posts() ) { the_post(); ?>
 		<div class="flex-item">
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<?php echo '<a href="' . get_the_permalink() . '">'; ?>
-			<?php the_title( '<h2>', '</h2>'); ?>
-			<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'small'); }  ?>
-			<?php simpleTheme_date(); ?>
-			<?php the_excerpt(); ?>
-			<?php echo '</a>'; ?>
+			<?php
+				echo '<a href="' . get_the_permalink() . '" class="image-zoom">';
+					the_title( '<h2>', '</h2>');
+					if ( has_post_thumbnail() ) {
+						echo '<div class="box-img">';
+						the_post_thumbnail( 'small');
+						echo '</div>';
+					}
+			 	echo '</a>';
+			 	simpleTheme_date();
+			 	the_excerpt();
+			 	echo '<div class="more-link-con"><a href="' . get_permalink() . '" class="more-link">' . __( 'Læs mere', 'simpletheme') . '</a></div>';
+			?>
 			</article>
 		</div>
 	<?php } ?>
--- template/sidebar/important.php
+++ template/sidebar/important.php
@@ -1,6 +1,10 @@
 <?php if ( is_active_sidebar( 'sidebar-important' ) || class_exists( 'WooCommerce' ) ) { ?>
 <div class="important-con">
 <?php dynamic_sidebar( 'sidebar-important' ); ?>
-<?php simpleTheme_small_cart() ?>
+<?php
+	if ( class_exists( 'WooCommerce' ) ) {
+  	simpleTheme_small_cart();
+	}
+?>
 </div>
 <?php } ?>
